4fe5ef1180cbdc89e4eb70da3a2fb9b5a7b98961
Donald Fries <donald.darklight@gmail.com>
2019-03-08 23:06:40 -0600

    modified for IIS hosted application compatibility

--- wiki.go
+++ wiki.go
@@ -9,12 +9,15 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+
+	"github.com/joho/godotenv"
 )
 
 // Page represents a page in the Wiki
 type Page struct {
-	Title string
-	Body  []byte
+	Prefix string
+	Title  string
+	Body   []byte
 }
 
 const (
@@ -33,13 +36,13 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &Page{Prefix: prefix, Title: title, Body: body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		http.Redirect(w, r, prefix+"/edit/"+title, http.StatusFound)
 		return
 	}
 	renderTemplate(w, "view", p)
@@ -48,20 +51,24 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		p = &Page{Title: title}
+		p = &Page{Prefix: prefix, Title: title}
 	}
 	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
+	p := &Page{Prefix: prefix, Title: title, Body: []byte(body)}
 	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	http.Redirect(w, r, prefix+"/view/"+title, http.StatusFound)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, r.URL.Path, http.StatusInternalServerError)
 }
 
 var templates = template.Must(template.ParseFiles(templatePath+"/edit.html", templatePath+"/view.html"))
@@ -73,25 +80,39 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
-			http.NotFound(w, r)
+			http.Error(w, r.URL.Path, http.StatusNotFound)
 			return
 		}
 		fn(w, r, m[2])
 	}
 }
 
+var prefix string
+var port string
+var validPath *regexp.Regexp
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	prefix = os.Getenv("APPLICATION_PREFIX")
+	port = os.Getenv("ASPNETCORE_PORT")
+	validPath = regexp.MustCompile("^" + prefix + "/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 	newpath := filepath.Join(".", pagesPath)
 	os.MkdirAll(newpath, os.ModePerm)
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	fmt.Println("listening for a connection at http://localhost:8080/view/test")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc(prefix+"/view/", makeHandler(viewHandler))
+	http.HandleFunc(prefix+"/edit/", makeHandler(editHandler))
+	http.HandleFunc(prefix+"/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", notFoundHandler)
+
+	fmt.Println("listening for a connection at http://localhost:" + port + prefix + "/view/test")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
